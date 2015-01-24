12ee100332fe50edeb2ca9ab6e4eabc4873823dc
choj <cho.james.c@Gmail.com>
2015-01-23 16:47:29 -0800

    added comments

--- WindaqReader/src/header.py
+++ WindaqReader/src/header.py
@@ -1,19 +1,21 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
 import struct
 
 class header(object):
-    header_extent = struct.Struct("<h")
-    value_8001H = struct.Struct("<h")
-    element27 = struct.Struct("<H")
+	# set new Struct objects with format strings. each object reads/writes binary data per format str
+    header_extent = struct.Struct("<h") # little endian short int, single 16-bit word (up to 2^15, one bit unused)
+    value_8001H = struct.Struct("<h") # "
+	
+	# element 27 refers to codas doc, where el 27 bit 14 specifies packedness
+    element27 = struct.Struct("<H") # little endian unsigned short int, single value
 
     def __init__(self,wholefile):
+		# set self.file = object for this instance of header
         self.file = wholefile
         self.get_extent()
 
-
-    def get_extent(self):
-        self.file.seek(6)
+		
+    def get_extent(self): # get number of bytes in header
+        self.file.seek(6) # go to 7th byte in file (first byte = 0)
         self.extent = header.header_extent.unpack(self.file.read(2))[0]
         return self.extent
 
@@ -23,7 +25,14 @@ class header(object):
         return self.values_8001H[0]
 
     def get_is_packed(self):
+		# go to 100th byte
         self.file.seek(100)
+		
+		# read in 2 bytes (bytes 100, 101) for element 27's entirety
         (whole_27,) = header.element27.unpack(self.file.read(2))
-        self.is_packed = whole_27 & 0x0002
+		
+        self.is_packed = whole_27 & 0x0002 # & does a bitwise "and", refer to https://wiki.python.org/moin/BitwiseOperators. 
+		# 0x0002 is two bytes in hex. binary = 0000 0000  0000 00010 (14th bit is 1, counting starts at 0)
+		# this is bitmasking https://en.wikipedia.org/wiki/Bitwise_operation#AND
+		
         return self.is_packed
