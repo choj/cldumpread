4b9f016a7bb23aad883e6dff714c5531876ec02d
choj <cho.james.c@Gmail.com>
2015-01-23 16:48:19 -0800

    comments

--- WindaqReader/src/windaqreader.py
+++ WindaqReader/src/windaqreader.py
@@ -1,6 +1,5 @@
 import os.path
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+import decimal
 
 __author__="hari"
 __date__ ="$May 20, 2009 11:13:44 AM$"
@@ -20,9 +19,10 @@ class Windaqreader(object):
         h = header(self.file)
         extent = h.get_extent()
         my8001h = h.get_value_8001H()
-        print "Header Bytes:%d" % extent
-        print "Value  8001H:%d" % my8001h
-        print "Is    Packed:%d" % h.get_is_packed()
+        print("Header Bytes:%d" % extent)
+        print("Value  8001H:%d" % my8001h)
+        print("Is    Packed:%d" % h.get_is_packed())
+		
     def get_slope(self):
         self.file.seek(110)
         self.file.read(4)
@@ -31,10 +31,10 @@ class Windaqreader(object):
         self.slope = Windaqreader.slope_struct.unpack(self.file.read(8))[0]
         self.intercept = Windaqreader.slope_struct.unpack(self.file.read(8))[0]
         self.tag = self.file.read(6)
-        print self.slope , self.intercept, self.tag
+        print(self.slope , self.intercept, self.tag)
         
     def print_data_file(self):
-        self.file.seek(1156)
+        self.file.seek(1156) # hardcoded for single channel?
         self.values = []
 
         try:
@@ -42,27 +42,25 @@ class Windaqreader(object):
                 data = self.file.read(2)
                 if(data == "" ):
                     break
-                val1 = Windaqreader.winvalue_struct.unpack(data)[0] >> 2
-                true = val1*self.slope + self.intercept
+                val1 = Windaqreader.winvalue_struct.unpack(data)[0] >> 2 # shift bits right 2 places
+                true = round(val1*self.slope + self.intercept, 6)
                 composite = []
-                composite.append(true * -1)
+                #composite.append(true * -1)
                 composite.append(true)
                 self.values.append(composite)
            
-        except Exception , e:
+        except Exception as e:
             #print "Caught Exception while printing list:%d values read : Error %s" % (i,e.message)
             pass
 
         import os
         self.outfile = open(os.path.splitext(self.file.name)[0] + "_outfile.csv" , "w")
-        csvwriter = csv.writer(self.outfile,dialect=csv)
+        csvwriter = csv.writer(self.outfile, dialect='excel', lineterminator='\n')
         for val in self.values:
          csvwriter.writerow(val)
         self.outfile.write("\n")
         self.outfile.flush()
         self.outfile.close()
-
-
 
 
 def main():
