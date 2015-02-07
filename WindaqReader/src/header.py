9793d7d429b114ef783af4cb7d48b26b08ebf125
choj <cho.james.c@Gmail.com>
2015-02-06 16:14:17 -0800

    Update header.py

--- WindaqReader/src/header.py
+++ WindaqReader/src/header.py
@@ -2,52 +2,60 @@ import struct
 
 class header(object):
 	# set new Struct objects with format strings. each object reads/writes binary data per format str
-    header_extent = struct.Struct("<h") # little endian short int, single 16-bit word (up to 2^15, one bit unused)
-    adc_extent = struct.Struct("<L") # little endian unsigned long int
-    value_8001H = struct.Struct("<h") # "
+	
+	# little endian short int, single 16-bit word (up to 2^15, one bit unused)
+	header_extent = struct.Struct("<h") 
+	value_8001H = struct.Struct("<h")
+	
+	# little endian unsigned long int
+	adc_extent = struct.Struct("<L") 
 	
 	# element 27 refers to codas doc, where el 27 bit 14 specifies packedness
-    element27 = struct.Struct("<H") # little endian unsigned short int, single value
-	
-	
+	# little endian unsigned short int, single value
+	element27 = struct.Struct("<H") 
 
-    def __init__(self,wholefile):
+	def __init__(self, wholefile):
 		# set self.file = object for this instance of header
-        self.file = wholefile
-        self.get_extent()
+		self.file = wholefile
+		self.get_extent()
 
 	# get number of channels
-    def get_chan_count(self):
-        if self.get_extent() == 1156:
-            #read chan count for STANDARD windaq file
-            self.file.seek(0)
-            self.chan_count = ord(self.file.read(1)) & ~(1 << 5)
-        else:
-            #read chan count for MULTIPLEXER windaq file
-            self.file.seek(0)
-            self.chan_count = ord(self.file.read(1))
-
-        return self.chan_count
+	def get_chan_count(self):
+		self.file.seek(1)
 		
+		#if self.get_extent() == 1156:
+		if ord(self.file.read(1)) == 0:
+			#read chan count for STANDARD windaq file
+			self.file.seek(0)
+			
+			# return value with 5th bit cleared: https://wiki.python.org/moin/BitManipulation
+			# per CODAS doc, 5th bit is set for STANDARD windaq files
+			# this works by doing a bitwise AND with the complement of 1 shifted left 5
+			# 0001 0000, complement: 1110 1111
+			self.chan_count = ord(self.file.read(1)) & ~(1 << 5)
+		else:
+			#read chan count for MULTIPLEXER windaq file
+			self.file.seek(0)
+			self.chan_count = ord(self.file.read(1))
 
-		
+		return self.chan_count
 	
-    def get_extent(self): # get number of bytes in header
-        self.file.seek(6) # go to 7th byte in file (first byte = 0)
-        self.extent = header.header_extent.unpack(self.file.read(2))[0]
-        return self.extent
+	def get_extent(self): # get number of bytes in header
+		self.file.seek(6) # go to 7th byte in file (first byte = 0)
+		self.extent = header.header_extent.unpack(self.file.read(2))[0]
+		return self.extent
 		
-    def get_adc_extent(self): # get number of bytes in header
-        self.file.seek(8) # go to 9th byte in file (first byte = 0)
-        self.adc_extent = header.adc_extent.unpack(self.file.read(4))[0] # read 4 bytes because a long is 32 bits
-        return self.adc_extent
+	def get_adc_extent(self): # get number of bytes in header
+		self.file.seek(8) # go to 9th byte in file (first byte = 0)
+		self.adc_extent = header.adc_extent.unpack(self.file.read(4))[0] # read 4 bytes because a long is 32 bits
+		return self.adc_extent
 
 		#element 6, byte 8-11, type UL, Unpacked Files: Number of ADC data bytes in file excluding header. Can be used to determine trailer start
 
-    def get_value_8001H(self):
-        self.file.seek(self.extent - 2)
-        self.values_8001H = header.value_8001H.unpack(self.file.read(2))[0]
-        return self.values_8001H
+	def get_value_8001H(self):
+		self.file.seek(self.extent - 2)
+		self.values_8001H = header.value_8001H.unpack(self.file.read(2))[0]
+		return self.values_8001H
 
 	
 	# get slopes of each channel
@@ -56,15 +64,15 @@ class header(object):
 	
 	# read event markers from trailer
 		
-    def get_is_packed(self):
+	def get_is_packed(self):
 		# go to 100th byte
-        self.file.seek(100)
+		self.file.seek(100)
 		
 		# read in 2 bytes (bytes 100, 101) for element 27's entirety
-        (whole_27,) = header.element27.unpack(self.file.read(2))
+		(whole_27,) = header.element27.unpack(self.file.read(2))
 		
-        self.is_packed = whole_27 & 0x0002 # & does a bitwise "and", refer to https://wiki.python.org/moin/BitwiseOperators. 
+		self.is_packed = whole_27 & 0x0002 # & does a bitwise "and", refer to https://wiki.python.org/moin/BitwiseOperators. 
 		# 0x0002 is two bytes in hex. binary = 0000 0000  0000 00010 (14th bit is 1, counting starts at 0)
 		# this is bitmasking https://en.wikipedia.org/wiki/Bitwise_operation#AND
 		
-        return self.is_packed
+		return self.is_packed
