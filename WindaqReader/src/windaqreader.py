25adff9013fb21c42c8f0bfc05cc731fe8c67559
choj <cho.james.c@Gmail.com>
2015-03-02 12:37:40 -0800

    added multichannel reading, output

--- WindaqReader/src/windaqreader.py
+++ WindaqReader/src/windaqreader.py
@@ -1,10 +1,7 @@
 import os.path
 import decimal
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
 
-__author__="hari"
-__date__ ="$May 20, 2009 11:13:44 AM$"
+
 from optparse import OptionParser
 from header import header
 import struct
@@ -14,16 +11,15 @@ class Windaqreader(object):
 
 	winvalue_struct = struct.Struct("<h") # little endian short int, single 16-bit word (up to 2^15, one bit unused)
 	slope_struct = struct.Struct("<d") # little endian double float, 32-bit
-	unit_struct = struct.Struct("<B") # little endian unsigned char
 	
 	def __init__(self,file):
 		self.file = open(file, "rb")
+		self.chan_count = header(self.file).get_chan_count()
 
 	def print_header(self):
 		h = header(self.file)
 		self.header_extent = h.get_extent()
 		self.adc_extent = h.get_adc_extent()
-		self.chan_count = h.get_chan_count()
 		my8001h = h.get_value_8001H()
 		print("Channel Count: %d" % self.chan_count)
 		print("Header Bytes: %d" % self.header_extent)
@@ -41,40 +37,59 @@ class Windaqreader(object):
 
 		self.slope = Windaqreader.slope_struct.unpack(self.file.read(8))[0]
 		self.intercept = Windaqreader.slope_struct.unpack(self.file.read(8))[0]
-		self.tag = self.file.read(6)
+		self.tag = self.file.read(4).decode('ascii')
+		
 		print(self.slope, self.intercept, self.tag)
+		
+	def get_slopes(self):
+		# get all slopes per channel count
+		self.slopes = []
+		self.intercepts = []
+		self.tags = []
+		for i in range(self.chan_count):
+			self.file.seek(118 + i*36)
+			self.slopes.append(Windaqreader.slope_struct.unpack(self.file.read(8))[0])
+			self.intercepts.append(Windaqreader.slope_struct.unpack(self.file.read(8))[0])
+			self.tags.append(self.file.read(4).decode('ascii'))
+			
+		print("\nch\tslope\t\tintercept\tunit")
+		for i in range(self.chan_count):
+			print('{}\t{:.6f}\t{:12.4f}\t{}'.format(i+1, self.slopes[i], self.intercepts[i], self.tags[i]))
+		print("\n")
 		
 	def print_data_file(self):
 	
 		# seek to end of header
 		self.file.seek(self.header_extent)
 		self.values = []
-
+		
+		# step thru data section, 2 bytes at a time
 		try:
-			#while True:
 			while self.file.tell() < self.header_extent + self.adc_extent:
-				data = self.file.read(2)
-				if(data == "" ):
-					break
-				# shift bits right 2 places per doc. i believe this knocks out the first two bits, which are state vars
-				val1 = Windaqreader.winvalue_struct.unpack(data)[0] >> 2
-				true = round(val1*self.slope + self.intercept, 6)
-				#print(true)
-				#print(self.file.tell())
-				composite = []
-				#composite.append(true * -1)
-				composite.append(true)
-				self.values.append(composite)
-		   
+				for i in range(len(self.slopes)):
+					data = self.file.read(2)
+					if(data == "" ):
+						break
+					# shift bits right 2 places per doc. i believe this knocks out the first two bits, which are state vars
+					val1 = Windaqreader.winvalue_struct.unpack(data)[0] >> 2
+					self.values.append(round(val1*self.slopes[i] + self.intercepts[i], 6))
+               
 		except Exception as e:
 			#print "Caught Exception while printing list:%d values read : Error %s" % (i,e.message)
 			pass
-
-		import os
+			
+		# write tab separated values
 		self.outfile = open(os.path.splitext(self.file.name)[0] + "_outfile.csv" , "w")
-		csvwriter = csv.writer(self.outfile, dialect='excel', lineterminator='\n')
+		csvwriter = csv.writer(self.outfile, dialect='excel', lineterminator='\n', delimiter = '\t')
+		val_list = self.chan_count * ['']
+		i = 0
+		
 		for val in self.values:
-			csvwriter.writerow(val)
+			val_list[i] = '{:.6f}'.format(val)
+			i += 1
+			if i == self.chan_count:
+				i = 0
+				csvwriter.writerow(val_list)
 		 
 		self.outfile.write("\n")
 		self.outfile.flush()
@@ -88,6 +103,7 @@ def main():
 	wq = Windaqreader(options.file)
 	wq.print_header()
 	wq.get_slope()
+	wq.get_slopes()
 	wq.print_data_file()
 
 
